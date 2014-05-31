5ee9e1b973f17b04132399acd54e32de7b5602e3
Tobias Grosch <tgrosch@embeddedfactor.com>
2014-05-31 23:43:48 +0200

    updated download handle

--- dl.py
+++ dl.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python2
-import requests, re, os, sys, math, time
+import requests, re, os, sys, math, time, argparse
 from ConfigParser import SafeConfigParser
 from xml.dom.minidom import parseString
 from pprint import pprint
@@ -20,21 +20,31 @@ class RingBuffer:
             return self.data[self.cur]
         return self.data[-1]
 
-def log(show, msg, suc):
+def log(show, msg, suc, argdata = {}):
+    if not argdata:
+        global args
+    else:
+        args = argdata["args"]
     note = "INFO"
     if not suc:
         note = "ERROR"
-    print "[%s] %s: %s" % (note, show, msg)
+    if not suc or args.verbose:
+        print "[%s] %s: %s" % (note, show, msg)
 
-def download_file(url, output):
-    global speed_limit
+def download_file(show, url, output, argdata = {}):
+    if not argdata:
+        global speed_limit
+        global args
+    else:
+        speed_limit = argdata["speed_limit"]
+        args = argdata["args"]
     resume_byte_pos = 0
     file_options = 'wb'
-    if os.path.isfile(output_file):
+    if os.path.isfile(output):
         file_options = 'a+b'
-        resume_byte_pos = os.path.getsize(output_file)
+        resume_byte_pos = os.path.getsize(output)
     resume_header = {'Range': 'bytes=%d-' % resume_byte_pos}
-    r = requests.get(url, headers=resume_header, stream=True)
+    r = requests.get(url, headers=resume_header, stream=True, verify=False)
     size = r.headers.get('content-length')
     if size is None:
         log(show, "file already downloaded", True)
@@ -59,101 +69,111 @@ def download_file(url, output):
                 speed = float(len(ticks.data))/(time.time() - ticks.get())
                 ticks.append(time.time())
                 waiting_time = 1024.0*(1.0/speed_limit - 1.0/speed)
-                print "\r%.1f %% - %.1f KB/s speed     " % (percent, speed),
+                if args.progress:
+                    print "\r%.1f %% - %.1f KB/s speed     " % (percent, speed),
                 if waiting_time > sleep_time:
                     sleep_time += 0.000001
                 if sleep_time > 0:
                     if waiting_time < sleep_time:
                         sleep_time -= 0.000001
-                    time.sleep(sleep_time)
+                    if sleep_time > 0:
+                        time.sleep(sleep_time)
+                    else:
+                        sleep_time = 0
 
-if not os.path.isfile("zdf.ini"):
-    log("config", "file is missing", False)
-    sys.exit(1)
+if __name__ == "__main__":
 
-parser = SafeConfigParser()
-parser.read('zdf.ini')
-try:
-    speed_limit = int(parser.get("user", "speed"))
-except:
-    speed_limit = 9999999999
-shows = parser.get("user", "shows").split(",")
-media_dir = parser.get("user", "media_dir")
-download_quality = parser.get("user", "quality")
-download_format = ".%s" % parser.get("user", "format")
+    parser = argparse.ArgumentParser(description='ZDF Mediathek Auto Downloader')
+    parser.add_argument("-v", "--verbose", help="print INFO messages", action="store_true")
+    parser.add_argument("-p", "--progress", help="print download progress", action="store_true")
+    args = parser.parse_args()
 
-url = parser.get("zdf", "url")
-search_url = parser.get("zdf", "search")
-xml_url = parser.get("zdf", "xml")
-download_prefix = parser.get("zdf", "download_prefix")
+    if not os.path.isfile("zdf.ini"):
+        log("config", "file is missing", False)
+        sys.exit(1)
 
-link_regex = re.compile('<a href=".*">', re.IGNORECASE)
-id_regex = re.compile('/(\d)+/')
+    parser = SafeConfigParser()
+    parser.read('zdf.ini')
+    try:
+        speed_limit = int(parser.get("user", "speed"))
+    except:
+        speed_limit = 9999999999
+    shows = parser.get("user", "shows").split(",")
+    media_dir = parser.get("user", "media_dir")
+    download_quality = parser.get("user", "quality")
+    download_format = ".%s" % parser.get("user", "format")
 
-for show in shows:
-    show = show.strip().lower()
-    resp = requests.post(
-        url + search_url, 
-        params = {'sucheText': show},
-    )
+    url = parser.get("zdf", "url")
+    search_url = parser.get("zdf", "search")
+    xml_url = parser.get("zdf", "xml")
+    download_prefix = parser.get("zdf", "download_prefix")
 
-    find_string = ">%s<" % show
-    parts = re.split(find_string, resp.text, flags=re.IGNORECASE)
-    if len(parts) < 2:
-        log(show, "not found in search engine", False)
-        continue
-    m = link_regex.findall(parts[0] + ">")
-    if m:
-        link = m[-1]
-        link = url + link.split('"')[1]
+    link_regex = re.compile('<a href=".*">', re.IGNORECASE)
+    id_regex = re.compile('/(\d)+/')
 
-        resp = requests.get(link)
+    for show in shows:
+        show = show.strip().lower()
+        resp = requests.post(
+            url + search_url, 
+            params = {'sucheText': show},
+        )
 
-        find_string = "%s.* vom " % show
+        find_string = ">%s<" % show
         parts = re.split(find_string, resp.text, flags=re.IGNORECASE)
-        try:
-            date = parts[1].split("<")[0]
-        except:
-            log(show, "not found within the show", False)
-            sys.exit(1)
+        if len(parts) < 2:
+            log(show, "not found in search engine", False)
             continue
-        show_dir = os.path.join(media_dir, show)
-        if not os.path.isdir(show_dir):
-            os.makedirs(show_dir)
-        output_file = os.path.join(show_dir, "%s-%s.%s" % (show, date, download_format))
-        m = link_regex.findall(parts[0])
+        m = link_regex.findall(parts[0] + ">")
         if m:
             link = m[-1]
             link = url + link.split('"')[1]
-            m = id_regex.search(link)
-            media_id = link[m.start(0)+1:m.end(0)-1]
-            
-            link = url + xml_url % media_id
-            resp = requests.get(link)
-            
-            xml_data = parseString(resp.text.encode("utf8"))
-            
-            download_link = ""
-            for element in xml_data.getElementsByTagName("formitaet"):
-                quality = element.getElementsByTagName("quality")
-                if quality:
-                    quality = quality[0].firstChild.nodeValue
-                if quality != download_quality:
-                    continue
-                download = element.getElementsByTagName("url")
-                if download:
-                    download = download[0].firstChild.nodeValue
-                print download
-                if download.endswith(download_format) and download.startswith(download_prefix):
-                    download_link = download
-                    break
-            
-            if download_link:
-                log(show, "snatch successful", True)
-                try:
-                    download_file(download_link, output_file)
-                except:
-                    log(show, "download failed", False)
-                continue
 
-    log(show, "not found", False)
+            resp = requests.get(link)
+
+            find_string = "%s.* vom " % show
+            parts = re.split(find_string, resp.text, flags=re.IGNORECASE)
+            try:
+                date = parts[1].split("<")[0]
+            except:
+                log(show, "not found within the show", False)
+                sys.exit(1)
+                continue
+            show_dir = os.path.join(media_dir, show)
+            if not os.path.isdir(show_dir):
+                os.makedirs(show_dir)
+            output_file = os.path.join(show_dir, "%s-%s.%s" % (show, date, download_format))
+            m = link_regex.findall(parts[0])
+            if m:
+                link = m[-1]
+                link = url + link.split('"')[1]
+                m = id_regex.search(link)
+                media_id = link[m.start(0)+1:m.end(0)-1]
+
+                link = url + xml_url % media_id
+                resp = requests.get(link)
+
+                xml_data = parseString(resp.text.encode("utf8"))
+
+                download_link = ""
+                for element in xml_data.getElementsByTagName("formitaet"):
+                    quality = element.getElementsByTagName("quality")
+                    if quality:
+                        quality = quality[0].firstChild.nodeValue
+                    if quality != download_quality:
+                        continue
+                    download = element.getElementsByTagName("url")
+                    if download:
+                        download = download[0].firstChild.nodeValue
+                    if download.endswith(download_format) and download.startswith(download_prefix):
+                        download_link = download
+                        break
+
+                if download_link:
+                    log(show, "snatch successful", True)
+                    try:
+                        download_file(show, download_link, output_file)
+                    except:
+                        log(show, "download failed", False)
+                    continue
+
+        log(show, "not found", False)
